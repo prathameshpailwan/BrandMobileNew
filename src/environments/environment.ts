9df9cf7bb8b3b0337a2626c452fe6f9030608e8c
prathameshpailwan <prathameshpailwan02@gmail.com>
2025-04-26 20:56:11 +0530

    environment file change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment={
 production: false,
-    BrandMobile : "https://localhost:7145/"
+    //BrandMobile : "https://localhost:7145/"
+    BrandMobile : "https://akbrandmobileapi-11.onrender.com"
 }
