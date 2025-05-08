27def3c3fb9cc2f4521e609b1f36d87e060d4217
prathameshpailwan <prathameshpailwan02@gmail.com>
2025-05-09 00:00:39 +0530

    https added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment={
 production: false,
     //BrandMobile : "https://localhost:7145/"
-    BrandMobile : "brandmobileapi-eqcqb7c4dkf6dka8.canadaeast-01.azurewebsites.net"
+    BrandMobile : "https://brandmobileapi-eqcqb7c4dkf6dka8.canadaeast-01.azurewebsites.net/"
 }
